649f7586ceff2bac5c1158d561b16bbf30265363
DimaSalem <demamohamad1998@gmail.com>
2024-05-07 17:36:33 +0300

    UPD files for lab 8

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 'use strict'
 
-let employees = [];
+let employeeIdCounter = 999;
+const employeeCards = document.getElementById("employeeCards");
+const myForm = document.getElementById("employeeForm");
 
-function employee(EmployeeID, FullName, Department, Level, ImageURL) {
-    this.employeeID = EmployeeID;
+
+function employee(FullName, Department, Level, ImageURL) {
+    this.employeeID = null;
     this.fullName = FullName;
     this.department = Department;
     this.level = Level;
     this.imageURL = ImageURL;
     this.Salary = null;
     this.netSalary();
+    this.generateEmployeeID();
 
-    employees.push(this);
+    this.render();
+
 }
 
+
+employee.prototype.generateEmployeeID = function () {
+    this.employeeID = employeeIdCounter++;
+}
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -37,31 +46,49 @@ employee.prototype.netSalary = function () {
 }
 
 employee.prototype.render = function () {
+    let employeeCardEl = document.createElement('fieldset');
 
-    const employeeListElement = document.getElementById("employeeList");
-    const listItem = document.createElement("li");
-    listItem.innerHTML = `${this.fullName} (Net: $${this.Salary})`;
+    let imageEl = document.createElement('img');
+    imageEl.src = this.imageURL;
+    employeeCardEl.appendChild(imageEl);
 
-    employeeListElement.appendChild(listItem);
+    let idEl = document.createElement('p');
+    idEl.textContent = `ID: ${employeeIdCounter}`;
+    employeeCardEl.appendChild(idEl);
+
+    let fullNameEl = document.createElement('p');
+    fullNameEl.textContent = `Full Name: ${this.fullName}`;
+    employeeCardEl.appendChild(fullNameEl);
+
+    let departmentEl = document.createElement('p');
+    departmentEl.textContent = `Department: ${this.department}`;
+    employeeCardEl.appendChild(departmentEl);
+
+    let levelEl = document.createElement('p');
+    levelEl.textContent = `Level: ${this.level}`;
+    employeeCardEl.appendChild(levelEl);
+    console.log(employeeCards);
+
+    employeeCards.appendChild(employeeCardEl);
 }
 
-function createEmployeesInstances() {
-    let emp1 = new employee(1000, 'Ghazi Samer', 'Administration', 'Senior', './image/person.png');
-    let emp2 = new employee(1001, 'Lana Ali', 'Finance', 'Senior', './image/person.png');
-    let emp3 = new employee(1002, 'Tamara Ayoub', 'Marketing', 'Senior', './image/person.png');
-    let emp4 = new employee(1003, 'Safi Walid', 'Administration', 'Mid-Senior', './image/person.png');
-    let emp5 = new employee(1004, 'Omar Zaid', 'Development', 'Senior', './image/person.png');
-    let emp6 = new employee(1005, 'Rana Saleh', 'Development', 'Junior', './image/person.png');
-    let emp7 = new employee(1006, 'Hadi Ahmad', 'Finance', 'Mid-Senior', './image/person.png');
+myForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    let fullName = event.target.fullName.value;
+    let department = event.target.department.value;
+    let level = event.target.level.value;
+    let imageURL = event.target.imageUrl.value;
+
+    let newEmployee = new employee(fullName, department, level, imageURL);
+
+    event.target.reset();
 
 }
 
-function renderEmployees() {
-    for (let i = 0; i < employees.length; i++) {
-        employees[i].render();
-    }
-}
 
-createEmployeesInstances();
-renderEmployees();
+
+
 
